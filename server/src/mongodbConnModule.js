513b32d1fe6b43827ff4ebfaca11467ad1eef0fe
sanket95 <sanket@stylabs.in>
2018-02-26 04:43:47 +0530

    mongo conn

--- server/src/mongodbConnModule.js
+++ server/src/mongodbConnModule.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = function() {
-	// mongoose.connect('mongodb://localhost:27017/cart');
-	mongoose.connect('mongodb://heroku_wzx292v1:ov4ufiasdkmta14k26g2jl3i9a@ds129010.mlab.com:29010/heroku_wzx292v1');
+	mongoose.connect('mongodb://localhost:27017/cart');
 	var db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error"));
 	db.once("open", function(callback){
